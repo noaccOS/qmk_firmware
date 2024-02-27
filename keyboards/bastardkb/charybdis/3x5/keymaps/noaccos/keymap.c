8a164cc33a4983a82f32fd9fcac4267d87be409f
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-02-27 17:45:41 +0100

    wip

--- keyboards/bastardkb/charybdis/3x5/keymaps/noaccos/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/noaccos/keymap.c
@@ -8,12 +8,79 @@ enum charybdis_keymap_layers {
     
 };
 
-#define LOWER MO(LAYER_LOWER)
-#define RAISE MO(LAYER_RAISE)
+enum planck_layers {
+  _MAIN,
+  _GAME,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _NUMPAD
+};
+
+enum unicode_names {
+  agrave,
+  egrave,
+  igrave,
+  ograve,
+  ugrave,
+  aacute,
+  eacute,
+  iacute,
+  oacute,
+  uacute,
+  Agrave,
+  Egrave,
+  Igrave,
+  Ograve,
+  Ugrave,
+  Aacute,
+  Eacute,
+  Iacute,
+  Oacute,
+  Uacute
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [agrave] = 0x00E0,
+  [egrave] = 0x00E8,
+  [igrave] = 0x00EC,
+  [ograve] = 0x00F2,
+  [ugrave] = 0x00F9,
+  [aacute] = 0x00E1,
+  [eacute] = 0x00E9,
+  [iacute] = 0x00ED,
+  [oacute] = 0x00F3,
+  [uacute] = 0x00FA,
+  [Agrave] = 0x00C0,
+  [Egrave] = 0x00C8,
+  [Igrave] = 0x00CC,
+  [Ograve] = 0x00D2,
+  [Ugrave] = 0x00D9,
+  [Aacute] = 0x00C1,
+  [Eacute] = 0x00C9,
+  [Iacute] = 0x00CD,
+  [Oacute] = 0x00D3,
+  [Uacute] = 0x00DA
+};
+
+#define LOWER  MO(LAYER_LOWER)
+#define RAISE  MO(LAYER_RAISE)
+#define ADJUST MO(LAYER_ADJUST)
 
 #define CTL_BSP CTL_T(KC_BSPC)
 #define SFT_SPC SFT_T(KC_SPC)
 #define GUI_ENT GUI_T(KC_ENT)
+
+#define KC_AC_A XP(aacute, Aacute)
+#define KC_AC_E XP(eacute, Eacute)
+#define KC_AC_I XP(iacute, Iacute)
+#define KC_AC_O XP(oacute, Oacute)
+#define KC_AC_U XP(uacute, Uacute)
+#define KC_GR_A XP(agrave, Agrave)
+#define KC_GR_E XP(egrave, Egrave)
+#define KC_GR_I XP(igrave, Igrave)
+#define KC_GR_O XP(ograve, Ograve)
+#define KC_GR_U XP(ugrave, Ugrave)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,11 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
        RGB_TOG, KC_MNXT, KC_MPLY, KC_MPRV, XXXXXXX,    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS,
+       KC_AC_A, KC_AC_O, KC_AC_E, KC_AC_U, KC_AC_I,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,  QK_BOOT,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS,
+       XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
+                         XXXXXXX, XXXXXXX, _______,     ADJUST, _______
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
@@ -45,11 +112,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
+       KC_GR_A,KC_GR_O,  KC_GR_E, KC_GR_U, KC_GR_I,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT, EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                         _______, _______, XXXXXXX,    _______, XXXXXXX
+                         KC_LALT, KC_LCTL,  ADJUST,    _______, XXXXXXX
+  //                   ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
+  [LAYER_ADJUST] = LAYOUT(
+  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_GR_A,KC_GR_O,  KC_GR_E, KC_GR_U, KC_GR_I,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
+  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
+       KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT, EE_CLR,  XXXXXXX, XXXXXXX, XXXXXXX,
+  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
+                         KC_LALT, KC_LCTL, XXXXXXX,    _______, XXXXXXX
   //                   ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
